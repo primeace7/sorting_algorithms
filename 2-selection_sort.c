14283bf10f773ca6e9168956d4e929d0a240e17a
primeace7 <osaruonamenjeffery@gmail.com>
2023-06-26 12:56:21 +0000

    Correct Betty errors in task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,28 +9,22 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_index;
-	int min_value;
+	size_t i, j;
+	int *min_value;
 
 	for (i = 0; i < size; i++)
 	{
-		min_value = array[i];
-		min_index = i;
+		min_value = array + i;
 
 		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < min_value)
-			{
-				min_index = j;
-				min_value = array[j];
-			}
-		}
+			if (array[j] < *min_value)
+				min_value = array + j;
 
-		if(min_index != i)
+		if (min_value != i + array)
 		{
-			array[min_index] = array[min_index] ^ array[i];
-			array[i] = array[i] ^ array[min_index];
-			array[min_index] = array[min_index] ^ array[i];
+			*min_value = *min_value ^ array[i];
+			array[i] = array[i] ^ *min_value;
+			*min_value = *min_value ^ array[i];
 			print_array(array, size);
 		}
 	}
